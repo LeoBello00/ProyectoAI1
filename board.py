f2668104b28da0056da4110bac3622521c1c94aa
Célien <celien.bonhomme@laposte.mail>
2024-03-06 15:32:54 -0500

    feat: play_game and board

--- board.py
+++ board.py
@@ -1,3 +1,16 @@
 class Board():
-    def __init__(self):
-        ...
+    def __init__(self, size=3):
+        self.size = size
+        self.grid = [[0 for _ in range(size)] for _ in range(size)]
+    
+    def __str__(self):
+        return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.board])
+    
+    def is_full(self):
+        pass
+
+    def get_score(self):
+        return 1
+    
+    def make_move(self, move, player):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 from board import Board
-from strategy import Random, Greedy, MinMax, AlphaBeta
+from strategy import Human, Random, Greedy, MinMax, AlphaBeta
 
 def play_game(strategy1, strategy2):
+    # Start the game
     print(f"{strategy1.__class__.__name__} VS {strategy2.__class__.__name__}")
+    while not board.is_full():
+        # Player 1
+        move = strategy1.next_move(board)
+        print(f"Player 1: {move}")
+        board.make_move(move, 1)
+        print(board)
+
+        if board.is_full():
+            if board.get_score() == 0:
+                print("It's a draw!")
+                return
+        if board.get_score() > 0:
+            print("Player 1 wins!")
+            return
+        if board.get_score() < 0:
+            print("Player 2 wins!")
+            return
+
+        # Player 2
+        move = strategy2.next_move(board)
+        print(f"Player 2: {move}")
+        board.make_move(move, 2)
+        print(board)
+
+
+    if board.get_score() == 0:
+        print("It's a draw!")
+        return
+    if board.get_score() > 0:
+        print("Player 1 wins!")
+        return
+    if board.get_score() < 0:
+        print("Player 2 wins!")
+        return
 
 if __name__ == '__main__':
     board = Board()
